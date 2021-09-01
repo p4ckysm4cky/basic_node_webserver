987805337f75f1bc25393fdcd5e1f4998843bd79
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2021-09-01 13:49:38 +1200

    Completed basic webserver

--- server.js
+++ server.js
@@ -1,8 +1,44 @@
 const http = require("http")
+const fs = require("fs")
+
+// if process.env.PORT exists it will use that instead
 const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-    console.log("Hello World")
+    res.setHeader("Content-Type", "text/html")
+
+
+    let path = "public/"
+    switch(req.url) {
+        case "/":
+            path += "index.html"
+            res.statusCode = 200
+            break
+        case "/about":
+            path += "about.html"
+            res.statusCode = 200
+            break
+        case "/contact-me":
+            path += "contact-me.html"
+            res.statusCode = 200
+            break
+        default:
+            path += "404.html"
+            res.statusCode = 404
+            break
+    }
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.write(data)
+            res.end()
+        }
+
+
+    })
+
 })
 
 server.listen(PORT, () => {
